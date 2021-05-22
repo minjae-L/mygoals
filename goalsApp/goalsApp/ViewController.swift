57f9efbaa36bdc15fa49bb1d1b5edcb073296d85
leeminjae <qlqlzbto@naver.com>
2021-05-22 22:06:57 +0900

    setting

---
+++ goalsApp/goalsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalsApp
+//
+//  Created by 이민재 on 2021/05/22.
+//  Copyright © 2021 이민재. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
